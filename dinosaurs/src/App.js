fd210e3e3572bfeaa9c601e17585a70157e0e809
ChiaraTorta <chiara.torta@mailbox.org>
2022-05-13 11:06:11 +0200

    initial commit

---
+++ dinosaurs/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(pros);
+    this.state = {
+      dinos: []
+    }
+  }
+  render() {
+    return (
+      <h1>Dinosaurs Dashboard</h1>
+    )
+  };
+}
+
+export default App;
